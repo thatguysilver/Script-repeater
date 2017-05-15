45870827deaadd8a2ecc7d4236aba4b063d7a342
Adam Silver <adampaulsilver@gmail.com>
2017-05-15 10:16:51 -0400

    Added user input

--- rpt.py
+++ rpt.py
@@ -1,9 +1,12 @@
 #! /usr/bin/python3
 
-import threading, os
+import threading, os, sys
 
 def repeat():
+    to_run= sys.argv[1]
+    
     threading.Timer(1.0, repeat).start()
-    os.system('~/py_projects/Random_world/main.py')
+    os.system("python3 " + to_run)
+
 repeat()
 
