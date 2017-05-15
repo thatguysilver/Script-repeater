7e8d0f18b6f88ab3a8f10105a2d266e2494837a5
Adam Silver <adampaulsilver@gmail.com>
2017-05-15 09:57:02 -0400

    Tested and ready for use.

--- rpt.py
+++ rpt.py
@@ -1,9 +1,9 @@
-#! usr/bin/python3
+#! /usr/bin/python3
 
 import threading, os
 
 def repeat():
     threading.Timer(1.0, repeat).start()
-    os.system('python3 test.py')
+    os.system('~/py_projects/Random_world/main.py')
 repeat()
 
