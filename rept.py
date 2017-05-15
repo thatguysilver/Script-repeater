da42bb61f67ec21f9ccd681e52b6b4a2db3e998c
thatguysilver <adampaulsilver@gmail.com>
2017-05-15 14:39:27 +0000

    Added an interval argument

--- rept.py
+++ rept.py
@@ -1,11 +1,15 @@
 #! /usr/bin/python3
 
+'''
+format: ./rept.py <command> <interval>
+'''
 import threading, os, sys
 
 def repeat():
-    to_run= sys.argv[1]
-    
-    threading.Timer(1.0, repeat).start()
+    to_run = sys.argv[1]                    #command
+    seconds = sys.argv[2]                   #interval
+   
+    threading.Timer(int(seconds), repeat).start()
     os.system("python3 " + to_run)
 
 repeat()
