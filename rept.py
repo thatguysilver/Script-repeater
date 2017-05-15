2ea06cda2a52635e5e7d59285e0e0b16d34535d5
Adam Silver <adampaulsilver@gmail.com>
2017-05-15 16:07:32 -0400

    added rust support via cargo

--- rept.py
+++ rept.py
@@ -6,11 +6,15 @@ format: ./rept.py <command> <interval>
 import threading, os, sys
 
 def repeat():
-    to_run = sys.argv[1]                    #command
-    seconds = sys.argv[2]                   #interval
-   
-    threading.Timer(int(seconds), repeat).start()
-    os.system("python3 " + to_run)
+    if sys.argv[1] == "cargo":
+        to_run = sys.argv[1] + ' ' + sys.argv[2]
+        seconds = sys.argv[3]
+        threading.Timer(int(seconds), repeat).start()
+        os.system(to_run)
+
+    else:
+        threading.Timer(int(seconds), repeat).start()
+        os.system("python3 " + to_run)
 
 repeat()
 
