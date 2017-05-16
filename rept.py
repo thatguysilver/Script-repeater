558bf8fc1d39c30918f01d0a76d7b4f2cd723fa9
Adam Silver <adampaulsilver@gmail.com>
2017-05-16 09:59:18 -0400

    Now continuously opens new browser

--- rept.py
+++ rept.py
@@ -12,23 +12,30 @@ except ImportError:
     print("You need Selenium for browser languages")
 
 def repeat():
+
     if sys.argv[1] == "cargo":
         to_run = sys.argv[1] + ' ' + sys.argv[2]
         seconds = sys.argv[3]
         threading.Timer(int(seconds), repeat).start()
         os.system(to_run)
 
-    elif sys.argv[1][-2:] == 'js':
-        print("{}".format(os.path.abspath(sys.argv[1])))
-        to_run = os.path.abspath(sys.argv[1])
+    elif sys.argv[1][-4:] == 'html':
+
+        print("Opening " + os.path.abspath(sys.argv[1]) + " in a new window.")
+        
+        seconds = sys.argv[2]
         browser = webdriver.Firefox()
-        browser.get('{}'.format(to_run))
+        browser.get('file://' + os.path.abspath(sys.argv[1]))
+        threading.Timer(int(seconds), repeat).start()
+        browser.navigate().refresh()
+
+
 
     else:
         to_run = sys.argv[1]
         seconds = sys.argv[2]
         threading.Timer(int(seconds), repeat).start()
-        os.system("python3 " + format(to_run))
+        os.system("python3 " + to_run)
 
 repeat()
 
