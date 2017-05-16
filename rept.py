852d557f183c00012ee0f15e54b32aaa90728f51
Adam Silver <adampaulsilver@gmail.com>
2017-05-16 10:58:14 -0400

    pythonized main file, added sys.exit for ImportError

--- rept.py
+++ rept.py
@@ -7,11 +7,12 @@ import threading, os, sys, webbrowser
 
 try:
     from selenium import webdriver
-
 except ImportError:
-    print("You need Selenium for browser languages")
+    print("Sorry! You'll have to install Selenium to use this.")
+    sys.exit()
 
 def repeat():
+    '''Reads the filetype and runs it. Currently supports .rs (using Cargo only), .py and .html.'''
 
     if sys.argv[1] == "cargo":
         to_run = sys.argv[1] + ' ' + sys.argv[2]
@@ -23,14 +24,11 @@ def repeat():
 
         print("Opening " + os.path.abspath(sys.argv[1]) + " in a new window.")
         
-        #seconds = sys.argv[2]
         browser = webdriver.Firefox()
         browser.get('file://' + os.path.abspath(sys.argv[1]))
-        #threading.Timer(int(seconds), repeat).start()
+        
         while True:
             browser.refresh()
-
-
 
     else:
         to_run = sys.argv[1]
